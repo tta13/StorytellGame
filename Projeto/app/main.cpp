8ebd0ee326d518a697110c57d018ebd6ced0a574
Tales Tomaz Alves <54077178+tta13@users.noreply.github.com>
2019-11-29 11:50:11 -0300

    Adicionando tela inicial ao jogo

--- Projeto/app/main.cpp
+++ Projeto/app/main.cpp
@@ -3,6 +3,7 @@
 #include <allegro5/allegro_native_dialog.h>
 #include <allegro5/allegro_audio.h>
 #include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_image.h>
 #include "classe.h"
 #include <string.h>
 #include <iostream>
@@ -21,22 +22,36 @@
 
 using namespace std;
 
-ALLEGRO_AUDIO_STREAM *musica = NULL; 
+ALLEGRO_AUDIO_STREAM *musicafundo = NULL; 
+// Variável representando a janela principal
+    
+ALLEGRO_DISPLAY *janela = NULL;
+ 
+// Variável representando a imagem
+ALLEGRO_BITMAP *imagem = NULL;
  
 int main(){
+ 
+    // Inicializa a Allegro
+    al_init();
+ 
+    // Inicializa o add-on para utilização de imagens
+    al_init_image_addon();
+ 
+    // Configura a janela
+    janela = al_create_display(512, 250);
+ 
+    // Carrega a imagem
+    imagem = al_load_bitmap("telainicial.png");
+ 
+    // Desenha a imagem na tela
+    al_draw_bitmap(imagem, 0, 0, 0);
+ 
+    // Atualiza a tela
+    al_flip_display();
+
 	//const uint32_t trash = 0, ledg1 = 3, ledg2 = 12, ledg3 = 48, ledg4 = 192;
   	uint32_t bt = 0;
-
-  	printf("Abrindo FPGA...\n");
-  	int dev = open("/dev/de2i150_altera", O_RDWR);
-	if(dev == -1){
-		cout << "bugou";
-		return -1;
-	}
-  	printf("Limpando memória da placa...\n");
-  	//write(dev, &trash, G_LEDS);
-  	
-   	char a[] = "a.wav", b[] = "b.wav";
   	bool audios_Inicializados[20]={false};
    	int minhaMatriz[20][3] = {1,2,3,
                              2,4,99,
@@ -58,19 +73,19 @@ int main(){
                              0,-1,-1,
                              0,-1,-1,
                              0,-1,-1}; 
-   int id = 0, oldId = 0;
-   int minhaEscolha = -1;
-   vector<linhas> mylines;
-   for(int i = 0; i < N_AUDIOS; i++){
-      mylines.push_back(linhas(i, minhaMatriz[i], ""));
-   }
-   cout << "Inicializei o vector de linhas" << endl;
-   for(int i=0; i<N_AUDIOS; i++){
+	int id = 0, oldId = 0;
+	int minhaEscolha = -1;
+	vector<linhas> mylines;
+	for(int i = 0; i < N_AUDIOS; i++){
+	   mylines.push_back(linhas(i, minhaMatriz[i], ""));
+	}
+	cout << "Inicializei o vector de linhas" << endl;
+	for(int i=0; i<N_AUDIOS; i++){
       char aux[10] = " ";
       int tales = i%10;
       if(i>9){
          aux[0]='1';
-         aux[1]= tales+ '0';
+         aux[1]= tales + '0';
       }
       else{
          aux[0] = i + '0';
@@ -78,8 +93,48 @@ int main(){
       strcat(aux, wave);
       cout << aux << endl;
       mylines[i].inicializar(aux);
-   }
-   while(id != -1){
+   	}
+  	printf("Abrindo FPGA...\n");
+  	int dev = open("/dev/de2i150_altera", O_RDWR);
+	if(dev == -1){
+		cout << "bugou";
+		return -1;
+	}
+  	printf("Limpando memória da placa...\n");
+  	//write(dev, &trash, G_LEDS);
+	bool test = true;
+	al_install_audio();
+	al_init_acodec_addon();
+	al_reserve_samples(1);
+	musicafundo = al_load_audio_stream("musicadefundo.wav", 4, 1024);
+	al_attach_audio_stream_to_mixer(musicafundo, al_get_default_mixer());
+	al_set_audio_stream_playmode(musicafundo, ALLEGRO_PLAYMODE_LOOP);
+	al_set_audio_stream_playing(musicafundo, true);
+  	while(test){
+      if(read(dev, &bt, BUTTONS)){
+		  	//cout << bt << endl;
+		  	 //cout << "opa";
+			switch(bt){
+				case 7:
+               		test = false;
+				    break;
+				case 11:
+               		test = false;
+				    break;
+				case 13:
+               		test = false;
+				    break;
+				case 14: 
+               		test = false;
+				    break;
+				 default:
+				 	break;
+			  }
+		 }
+   	}
+   	// Finaliza a janela
+    al_destroy_display(janela);
+  	while(id != -1){
       sleep(2);
       cout << "ID igual a " << id << endl;
       //poe audio no buffer lido pelo drive de som
@@ -125,23 +180,21 @@ int main(){
 				    opa = false;
 				    //al_rewind_audio_stream(mylines[id].som);
 				    break;
-				 
 				 default:
 				 	break;
 			  }
-		 }
-		
-	}
-  if(id==99){
-    id = oldId;
-  }
-      
+		   }
+		}
+  		if(id==99){
+    		id = oldId;
+  		}
    }
-    //al_destroy_sample(som_azul);
-    //al_destroy_sample(som_verm);
+   al_set_audio_stream_playing(musicafundo, false);
+   al_detach_audio_stream(musicafundo);
    for(int i=0; i<N_AUDIOS; i++){
       al_destroy_audio_stream(mylines[i].som);
    }
+   	al_destroy_audio_stream(musicafundo);
 	close(dev);
     return 0;
 }
